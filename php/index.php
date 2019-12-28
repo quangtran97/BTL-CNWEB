ccb3be8fe67463e2c7043bab09b1fd015dee4547
quang-tran97 <58326741+quang-tran97@users.noreply.github.com>
2019-12-28 09:58:03 +0700

    bai cuoi ky

---
+++ php/index.php
@@ -0,0 +1,255 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>QUẢN LÝ ĐIỂM HỌC PHẦN</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="../css/layout.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+  
+</head>
+<body>
+<nav class="navbar navbar-inverse  navbar-fixed-top ">
+  <div class="container">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+      <a class="navbar-brand" href="index.php"><img src="../images/logothuyloi4.png"  style="width:40px;"  class="img-responsive" alt="mega"></a>
+    </div>
+    <div class="collapse navbar-collapse " id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"> Home</span></a></li>
+        <li class=""><a href="#"><span class=""> Giới Thiệu</span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tin Tức & Thông Báo<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Tin Tức</a></li>
+              <li><a href="#">Thông Báo</a></li>
+              <li><a href="#">Tin Video</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="diemhoctap.php" class="dropdown-toggle" data-toggle="dropdown">Điểm Học Tập<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+              <li><a href="diemhoctap.php">Điểm trung bình học kỳ</a></li>
+              <li><a href="diemhoctap.php">Điểm trung bình năm học</a></li>
+              <li><a href="diemhoctap.php">Điểm trung bình toàn khóa</a></li>
+              
+              
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Đào Tạo<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Đại Học Chính Quy</a></li>
+              <li><a href="#">Liên Thông Đại Học</a></li>
+              <li><a href="#">Tiến Sĩ</a></li>
+              <li><a href="#">Thạc Sĩ</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sinh Viên<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+
+              <li><a href="#">Thông Báo</a></li>
+              <li><a href="#">Quy Chế Quy Định</a></li>
+              <li><a href="#">Học Phí</a></li>
+
+       <li><a href="#">Đăng Kí Học</a></li>
+      <li><a href="diemsv.php">Tra Cứu Điểm Học Tập</a></li>
+     <li><a href="#">Tra Cứu Lịch Thi</a></li>
+     <li><a href="user.php">Thông Tin Sinh Viên</a></li>
+     
+
+             
+          </ul>
+        </li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="dangnhap.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+<div class="container text-center"> 
+  <div class="row content">
+  <a href="#"><img src="../images/thuyloi.png" alt="chuyendong1"></a><br><br>
+  
+    <div class="col-sm-8 text-left">
+   
+      <div class="well"><div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#myCarousel" data-slide-to="4"></li>
+        </ol>
+      
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <a href="#"><img src="../images/daotaotiengnhat.jpg" alt="daotaotiengnhat"></a>
+            <div class="carousel-caption">
+              <h3>ĐẠI HỌC THỦY LỢI</h3>
+              <p>Trường Đại học Thủy lợi mở rộng hợp tác đào tạo Tiếng Nhật và các kỹ năng nghề nghiệp cho sinh viên</p>
+            </div>
+          </div>
+      
+          <div class="item">
+            <a href="#"><img src="../images/letraobang.jpg" alt="letraobang"></a>
+            <div class="carousel-caption">
+              <h3 >ĐẠI HỌC THỦY LỢI</h3>
+              <p>Lễ tốt nghiệp và trao bằng tiến sỹ,thạc sỹ đợt 2 năm 2019</p>
+            </div>
+          </div>
+      
+          <div class="item">
+            <a href="#"><img src="../images/chuyendong3.jpg" alt="chuyendong3"></a>
+            <div class="carousel-caption">
+              <h3>ĐẠI HỌC THỦY LỢI</h3>
+              <p>Đội tuyển bóng đá nam đại học Thủy Lợi quyết tâm giành HCV tại giải sinh viên toàn quốc</p>
+            </div>
+          </div>
+          <div class="item">
+            <a href="#"><img src="../images/xuthechonnganh.jpg" alt="xuthechonnganh.jpg"></a>
+            <div class="carousel-caption">
+              <h3>ĐẠI HỌC THỦY LỢI</h3>
+              <p>Xu thế chọn ngành của thí sinh năm 2019 tại cơ sở Hà Nội</p>
+            </div>
+          </div>
+          <div class="item">
+            <a href="#"><img src="../images/doituyentin.jpg" alt="doituyentin.jpg"></a>
+            <div class="carousel-caption">
+              <h3>Đại Học Thủy Lợi</h3>
+              <p>Đội tuyển Olympic Tin học sinh viên tạo dấu ấn tại kỳ thi toàn quốc lần thứ 28</p>
+            </div>
+          </div>
+        </div>
+      
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div></div>
+     
+      <h1 class="text-danger">Tuyển Sinh</h1><br>
+      <div class="well"><div class="bg-info"> <div class="container-fluid bg-4 text-center">   
+            
+            <div class="row">
+               <div class="col-sm-3"> 
+                  <a href="#"><img src="../images/tuyensinh1.jpg" class="img-responsive" style="width:100%"  alt="tuyensinh1"><p>Thông báo tuyển sinh văn bằng 2...</p></a>
+                </div>
+              <div class="col-sm-3"> 
+                <a href="#"><img src="../images/tuyensinh2.jpg" class="img-responsive" style="width:100%"  alt="tuyensinh2"><p>Trình độ đào tạo thạc sĩ đợt 2...</p></a>
+              </div>
+              <div class="col-sm-3"> 
+                <a href="#"><img src="../images/tuyensinh3.jpg" class="img-responsive" style="width:100%" alt="tuyensinh3"><p>Tuyển  sinh liên thông đại học...</p></a>
+              </div>
+              <div class="col-sm-3">
+                    <a href="#"><img src="../images/tuyensinh4.jpg" class="img-responsive" style="width:100%" alt="tuyensinh4"><p>Tuyển sinh thẳng các sinh viên THPT ...</p></a>
+              </div>
+              
+            </div>
+          </div><br></div></div>
+       <h1 class="text-danger">Đào Tạo</h1><br>
+       <div class="well"><div class="bg-info"> <div class="container-fluid bg-4 text-center">    
+            
+            <div class="row">
+            <div class="col-sm-3">
+                    <a href="#"><img src="../images/daotao3.jpg" class="img-thumbnail" style="width:100%" alt="da0tao1"><p>Thông báo kế hoạch học kì 2 năm học 2019-2020</p></a>
+                </div>
+                <div class="col-sm-3"> 
+                    <a href="#"><img src="../images/daotao2.jpg" class="img-thumbnail" style="width:100%"  alt="da0tao2"><p>Thông báo lịch thi bằng tiếng anh A2</p></a>
+                </div>
+                <div class="col-sm-3"> 
+                    <a href="#"><img src="../images/daotao3.jpg" class="img-thumbnail" style="width:100%" alt="da0tao3"><p>thông tin luận án tiến sĩ cấp trường</p></a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#"><img src="../images/daotao2.jpg" class="img-thumbnail" style="width:100%" alt="da0tao4"><p>Lịch thi giai đoạn 2-học kì 1 năm học 2019-2020</p></a>
+                </div>
+                
+                </div>
+              </div><br></div></div> 
+        <h1 class="text-danger">Đối Ngoại</h1><br>
+        <div class="well"><div class="bg-info"> <div class="container-fluid bg-4 text-center">    
+            
+            <div class="row">
+            <div class="col-sm-3">
+                    <a href="#"><img src="../images/doingoai5.jpg" class="img-thumbnail" style="width:100%" alt="doingoai1"><p>Hợp tác với trường đại học Jeonju,mở rộng cơ hội phát triển</p></a>
+                </div>
+                <div class="col-sm-3"> 
+                    <a href="#"><img src="../images/doingoai2.jpg" class="img-thumbnail" style="width:100%"  alt="doingoai2"><p>Ảnh hưởng của công nghiệp 4.0</p></a>
+                </div>
+                <div class="col-sm-3"> 
+                    <a href="#"><img src="../images/doingoai3.jpg" class="img-thumbnail" style="width:100%" alt="doingoai3"><p>Bí Thư thứ nhất Hungary thăm và làm việc tại trường Đại học Thủy lợi</p></a>
+                </div>
+                <div class="col-sm-3">
+                    <a href="#"><img src="../images/doingoai6.jpg" class="img-thumbnail" style="width:100%" alt="doingoai4"><p>Trường đại học Thủy Lợi mở rộng hợp tác với đại học Hồ Hải -Trung Quốc</p></a>
+                </div>
+                
+                </div>
+              </div><br></div></div> 
+    </div>
+   
+    <div class="col-sm-4 sidenav">
+      <h1 class="text-danger">Sự Kiện</h1><br>
+      <a href="#"><img src="../images/sukien.jpg" class="img-responsive" style="width:100%" alt="sk1">INTERNATIONAL SYMPOSIUM</a>
+      <a href="#"><img src="../images/sukien2.png" class="img-responsive" style="width:100%" alt="sk2">APAC 2019</a>
+      <a href="#"><img src="../images/sukien3.png" class="img-responsive" style="width:100%" alt="sk3">HỘI NGHỊ KHOA HỌC</a>
+      <h1 class="text-danger">Thông Báo</h1><br>
+      <a href="#"><img src="../images/daotao4.jpg" class="img-responsive" style="width:70%" alt="thongbao1">Thông báo các môn thi trong chương trình kì I</a>
+      <h1 class="text-danger">Khoa Học Và Công Nghệ</h1><br>
+      <a href="#"><img src="../images/khoahoc.jpg" class="img-responsive" style="width:100%" alt="khcn1">Chuyên gia đại học Thủy Lợi nhận định "các đập thủy điện luôn phải dự báo các điều kiện cực đoan"</a>
+      <a href="#"><img src="../images/khoahoc2.jpg" class="img-responsive" style="width:100%" alt="khcn2">Cách Mạng Công Nghiệp 4.0 </a>
+     
+      
+       
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid  ">
+<div class="col-sm-4 text-left">
+  <img src="../images/logotlu.png" style="width:350px;  height:100px;">
+  </div>
+  <div class="col-sm-8 text-left">
+  
+<table style="width:50%">
+  
+  <tr>
+    <td><a href="#">Giới Thiệu</td>
+    <td><a href="#">Điều Khoản </td>
+    <td><a href="#">Liên Hệ</td>
+  </tr>
+  <tr>
+  <td><a href="#">Quảng Cáo</td>
+  <td><a href="#">Ứng Dụng</td>
+  <td><a href="#">Góp Ý</td>
+  </tr>
+  <tr>
+  <td><a href="#">Copyright</td>
+  <td><a href="#">FAQs</td>
+ 
+  </tr>
+</table>
+</div>
+  </div>
+</footer>
+
+</body>
+</html>
