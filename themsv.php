3c46841915f2a1f7223eb3dd85eea353c0dc1952
quangtran97 <59300111+quangtran97@users.noreply.github.com>
2020-01-07 22:40:13 +0700

    Add files via upload

---
+++ themsv.php
@@ -0,0 +1,140 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username'])&&($_SESSION['quyen'])==1){//tồn tại session thì mới hiển thị trang này
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>QUẢN LÝ SINH VIÊN</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="../css/layout.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+</head>
+<body>
+<?php
+  require('connect.php');
+  ?>
+<nav class="navbar navbar-inverse  navbar-fixed-top ">
+  <div class="container">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="index.php"><img src="../images/logothuyloi4.png"  style="width:40px;"  class="img-responsive" alt="mega"></a>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+      <li><a href="danhsachsv.php">Danh Sách Sinh Viên</a></li>
+      <li><a href="quanlydiem.php">Quản Lý Điểm</a></li>
+      <li><a href="quanlysv.php">Quản Lý Sinh Viên</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+     
+      <li><a href="dangxuat.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
+        
+    </div>
+  </div>
+</nav>
+
+<div class="container text-center">    
+  <div class="row content">
+    <h2 class="text-info">THÊM THÔNG TIN SINH VIÊN</h2>
+    <div >
+        <?php
+            if(isset($_POST['submit'])){
+                if(empty($_POST['Tensinhvien']) || empty($_POST['MSV'])|| empty($_POST['Lớp'])||empty($_POST['Khoa'])){
+                  
+                    echo '<p style="color:red">Vui lòng nhập đầy đủ thông tin</p>';
+                  }else{//nếu điền đầy đủ thông tin thì update
+                    $Tensinhvien=$_POST['Tensinhvien'];
+                    $MSV=$_POST['MSV'];
+                    $Lớp=$_POST['Lớp'];
+                    $Khoa=$_POST['Khoa'];
+                   
+                   
+                     $sql="select * from danhsachsv where MSV='$MSV'";
+                     $query= mysqli_query($con,$sql);
+                     $num =mysqli_num_rows($query);
+                     if($num==1){
+                        echo '<p style="color:red">Mã sinh viên đã tồn tại</p>';
+                     }else{
+                    $sql2= "INSERT INTO danhsachsv (Tensinhvien, MSV, Lớp,Khoa) VALUES ('$Tensinhvien', '$MSV', '$Lớp','$Khoa')";
+                    $query2=mysqli_query($con,$sql2);
+                   
+                    if($query2){
+                     
+        
+                        echo"<script type='text/javascript'>";
+                        echo"alert('Thêm dữ liệu sinh viên thành công')";
+                       
+                        echo"</script>";
+                    }
+                     }
+                    } 
+                }      
+            
+        ?>
+        
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+    <form action="" method="POST">
+        <div class="form-group">
+            <label  for="">Tên Sinh Viên:</label>
+            <input type="text" class="form-control" name="masv" placeholder="Tên Sinh Viên..." >
+            <label for="">Mã Sinh Viên:</label>
+            <input type="text" class="form-control" name="tensv" placeholder="Mã Sinh Viên..." >
+            <label for="">Lớp:</label>
+            <input type="text" class="form-control" name="tenlop" placeholder="Lớp..." >
+            <label for="">Khoa:</label>
+            <input type="text" class="form-control" name="tenlop" placeholder="Khoa..." >
+           
+        </div>
+        <button type="submit" name="submit" class="btn btn-danger">Thêm</button> 
+      </form>
+  </div>
+</div>
+</div>
+<footer class="container-fluid  ">
+<div class="col-sm-4 text-left">
+  <img src="../images/logotlu.png" style="width:350px;  height:100px;">
+  </div>
+  <div class="col-sm-8 text-left">
+  
+<table style="width:50%">
+  
+  <tr>
+    <td><a href="#">Giới Thiệu</td>
+    <td><a href="#">Điều Khoản </td>
+    <td><a href="#">Liên Hệ</td>
+  </tr>
+  <tr>
+  <td><a href="#">Quảng Cáo</td>
+  <td><a href="#">Ứng Dụng</td>
+  <td><a href="#">Góp Ý</td>
+  </tr>
+  <tr>
+  <td><a href="#">Copyright</td>
+  <td><a href="#">FAQs</td>
+ 
+  </tr>
+</table>
+</div>
+  </div>
+</footer>
+
+</body>
+</html>
+<?php
+    }else{
+      header('location:themsv.php');
+    }
+?>
